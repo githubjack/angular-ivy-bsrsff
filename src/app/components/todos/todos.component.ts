2d8ddd776e17641bc57ff385e789db4f0924a6e3
githubjack <jack.ui.maynard@gmail.com>
2022-01-26 18:02:25 +0000

    2 files modified

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -25,11 +25,15 @@ this.todos = [
   ]
 }
 
-toggleDone (id) {
+toggleDone (id:number) {
 this.todos.map((v, i) => {
   if(i == id) v.completed = !v.completed;
   return v;
 })
+}
+
+deleteTodo (id:number) {
+  this.todos = this.todos.filter((v, i) => i !== id);
 }
 
 }
