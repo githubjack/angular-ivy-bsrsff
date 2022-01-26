b2fc0d27f8de47e541e25312fa52330c8ec3bcfa
githubjack <jack.ui.maynard@gmail.com>
2022-01-26 17:44:42 +0000

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, TodosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
